f7c03c94621966c988901eb3ecd10dff338dfd66
gayoung-lee0921 <dudnrkdud1@gmail.com>
2022-11-07 16:52:17 +0900

    responsive header end

--- js/common.js
+++ js/common.js
@@ -16,28 +16,33 @@ tabMenu.on("click", function () {
 const btnAll = $(".btn-all");
 const gnb = $("#gnb");
 btnAll.on("click", function (e) {
+  // btn-all이 a태그 라서
   e.preventDefault();
   const icon = $(this).find(".material-icons");
 
-  if (gnb.hasClass("on")) {
-    icon.text("menu");
-  } else {
-    icon.text("close");
-  }
-
+  // gnb에 클래스 on을 붙였다 뗐다 할 수 있도록 만듦
   gnb.toggleClass("on");
+
+  // 이 때 클래스 on을 갖고 있다면 icon의 내용을 close로 바꿈, 없으면 menu로 바꿈
+  if (gnb.hasClass("on")) {
+    icon.text("close");
+  } else {
+    icon.text("menu");
+  }
 });
 
 //js에서 크기를 제어
 $(window).on("resize", function () {
   // 스크롤의 너비까지 포함해서 width 길이를 구하기 위해선 .outerWidh()를 해줘야 함
   const w = $(window).outerWidth();
-  console.log(w);
+  // console.log(w);
   if (w > 1280) {
     $("html").removeClass("m").addClass("pc");
+    // w < 1280보다 작을 땐 depth02에 style:block이라고 표시 됨
     $("#gnb .depth02").removeAttr("style");
-    $("#gnb").removeAttr("on");
+    $("#gnb").removeClass("on");
     const icon = $(".btn-all .material-icons");
+    // w < 1280보다 작을 땐 .btn-all의 텍스트 내용이 close였음
     icon.text("menu");
 
     // gnb에 on을 제거하고 tnb-all 안에 있는 material-icons의 텍스트 바꿔서 원래대로 바꾸기(menu로 바꾸기)
@@ -49,6 +54,7 @@ $(window).on("resize", function () {
 const depth01 = $("#gnb .depth01");
 
 depth01.on("click", function (e) {
+  e.preventDefault();
   if ($("html").hasClass("m")) {
     const depth02 = $(this).next();
     const siblings = $(this).parent().siblings().find(".depth02");
@@ -57,7 +63,6 @@ depth01.on("click", function (e) {
     siblingsDepth01.removeClass("on");
     siblings.slideUp();
     if (depth02.length > 0) {
-      e.preventDefault();
       depth02.stop().slideToggle();
     }
   }
